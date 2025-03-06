ecf33400589a59e4d81ee8dc37504d629ae2f4b7
ksgowrii <73364258+ksgowrii@users.noreply.github.com>
2025-03-06 22:22:26 +0530

    Add files via upload
    class room activity of week 1 day 1 is uploaded

---
+++ week1/day1/LearnDatatypes.java
@@ -0,0 +1,19 @@
+package week1.day1;
+
+public class LearnDatatypes {
+
+	public static void main(String[] args) {
+		//data types for car fields
+		
+		int regno = 498392;
+		boolean isavan = false;
+		float speed = 1344.23424234f;
+		String carname = "BMW";
+		char logo = 'B';
+        long phnumber = 1987863434999998L;
+        double capacity = 234.443433434;
+        System.out.println("the regno of the car is "+regno);
+        System.out.println("the name of the car is "+carname);
+        System.out.println("speed of the car named as " +carname + " is " +speed + " which has a capacity of " +capacity);
+	}
+}
